b56057972ff2e8c8a279ed626afd842dfe922178
Swadling <swadling10@gmail.com>
2025-03-15 16:08:04 -0400

    frames

--- frames.py
+++ frames.py
@@ -1,1 +1,32 @@
+import cv2
+
+def FrameCapture(path): 
+
+	# Path to video file 
+	vidObj = cv2.VideoCapture(path) 
+
+	# Used as counter variable 
+	count = 0
+
+	# checks whether frames were extracted 
+	success = 1
+
+	while success: 
+
+		# vidObj object calls read 
+		# function extract frames 
+		success, image = vidObj.read() 
+
+		# Saves the frames with frame-count 
+		cv2.imwrite("frame%d.jpg" % count, image) 
+
+		count += 1
+		print(count)
+        
+
+# Driver Code 
+if __name__ == '__main__': 
+
+	# Calling the function 
+	FrameCapture("videoplayback.mp4") 
 

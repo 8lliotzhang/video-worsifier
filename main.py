f139fe0ad4eff75f812d79b4cd617b1b00925aee
8lliotzhang <59445745+8lliotzhang@users.noreply.github.com>
2025-03-15 18:53:05 +0000

    main.py stop freezing pls

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-    #can u see this
-    #sjdaioajsoifsajiofjaiofjfoiajfoi
+import pywhatkit as pwk
+if __name__ == "__main__":
+    input_image = "bwtest-sm.jpg"
+    output_image = "ascii_out"
+
+    pwk.image_to_ascii_art(input_image,output_image)
+
+    with open(output_image+".txt","r") as f:
+        print(f.read())

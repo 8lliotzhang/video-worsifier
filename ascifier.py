7daf854c913d51489a78003a7b3805784b47acfa
8lliotzhang <59445745+8lliotzhang@users.noreply.github.com>
2025-03-15 20:23:59 +0000

    lets see if this works. just prints out every fram e

--- ascifier.py
+++ ascifier.py
@@ -1,9 +1,29 @@
+import os
 from ascii_magic import AsciiArt
 #HATE. LET ME TELL YOU HOW
 
 
 # take frame i of input
 # output frame i of ascii
+
+def convertAllFrames():
+    totalFrames = 301
+    AsciiArray = []
+
+    for i in range(totalFrames):     
+        filename = f'frame{i}.jpg'
+        if os.path.exists(filename):
+            print(filename + "exists yayayay")
+            AsciiArray[i].append(AsciiArt.from_image(filename))
+        else:
+            print(filename + " does not exist :(")
+            
+
+
+
+
+
+
 
 my_art = AsciiArt.from_image('erm.png')
 #replace with CURRENT FRAME
@@ -15,3 +35,4 @@ output = my_art.to_ascii()
 print(output) 
 #temp output - plan to STORE each frame, then display it frame by frame
 
+convertAllFrames()
